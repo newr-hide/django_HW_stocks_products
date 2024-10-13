da18b060dbd5ae588005e053523649e0990732a6
Bondarev Artem <bond210484@mail.ru>
2024-10-13 15:56:20 +0700

    add serializer 1 part

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,18 +1,27 @@
 from rest_framework import serializers
 
+from logistic.models import Product, Stock, StockProduct
+
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta():
+        model = Product
+        fields = ['id', 'title', 'description']
+
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta():
+        model = StockProduct
+        fields = ['stock', 'product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
+    class Meta():
+        model = Stock
+        fields = ['id', 'address', 'products', 'positions']
 
     # настройте сериализатор для склада
 
