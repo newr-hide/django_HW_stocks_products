3453b4223a53819ce4695faf098f3869075d46f8
Bondarev Artem <bond210484@mail.ru>
2024-10-20 11:27:19 +0700

    finish commit

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,7 +1,7 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 
 from logistic.models import Product, Stock, StockProduct
-
+from rest_framework.exceptions import ValidationError
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -10,33 +10,46 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description']
 
 
+class PositionSerializer(serializers.Serializer):
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    quantity = serializers.IntegerField(min_value=1)
+
+    def validate(self, data):
+        if not all([data['product'], data['quantity']]):
+            raise ValidationError({'error': 'Missing required fields: product or quantity.'})
+        return data
+
+
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     class Meta():
         model = StockProduct
-        fields = ['stock', 'product', 'quantity', 'price']
+        fields = ['product', 'quantity', 'price']
+
+
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
+    
     class Meta():
         model = Stock
         fields = ['id', 'address', 'products', 'positions']
 
-    # настройте сериализатор для склада
+
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
 
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+        for i in positions:
+            StockProduct.objects.create(stock=stock, **i)
 
         return stock
+
+
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
@@ -45,8 +58,13 @@ class StockSerializer(serializers.ModelSerializer):
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
 
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for i in positions:
+            obj, created = StockProduct.objects.update_or_create(
+                stock=stock,
+                product=i['product'],
+                defaults={'stock': stock, 'product': i['product'], 'quantity': i['quantity'],
+                          'price': i['price']}
+            )
         return stock
+
+
